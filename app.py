dacf0dd4cb214fc0df4c09fd878044a9fd53cd05
jasonscotch <jason.m.scot@gmail.com>
2023-04-09 14:52:24 -0700

    removed debugger and added config file

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request, redirect
-from flask_debugtoolbar import DebugToolbarExtension
+
 from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "chickenzarecool21837"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-debug = DebugToolbarExtension(app)
+
+app.config.from_pyfile('config.py')
 
 connect_db(app)
 db.create_all()
